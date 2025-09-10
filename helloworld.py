9f5d71ad6833f1e39e3be0a92a51a7c56d0e8072
pbogii <palotas.boglarka@diak.szbi-pg.hu>
2025-09-10 07:24:22 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print ("hello!")
+
+print ("Enter your last name: ")
+lastname = input ()
+
+print ("Enter your first name: ")
+firstname = input ()
+print ("Hello, " + lastname + " " + firstname + "!")
+print (f"hello {lastname} {firstname} !")
